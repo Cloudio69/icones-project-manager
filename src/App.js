766a56e6f12fe1edc78a0b666a37018f78b9c06a
Claudio <63803118+Cloudio69@users.noreply.github.com>
2022-06-24 15:42:05 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Wrapper, Card } from "./container/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ic-app">
+      <h1 className="p-4 text-3xl font-bold"> I tuoi promemoria </h1>
+      <div className="title-breaker px-5">
+        <hr />
+      </div>
+      <Wrapper>
+        <Card />
+      </Wrapper>
     </div>
   );
 }
